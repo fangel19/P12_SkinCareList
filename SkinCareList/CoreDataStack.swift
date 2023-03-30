ae1a3402c98fb86e178e775a79e560c90fe7d190
fangel19 <fournyangel@msn.com>
2023-03-30 19:22:40 +0200

    Debut de la CoreDataStack

---
+++ SkinCareList/CoreDataStack.swift
@@ -0,0 +1,40 @@
+//
+//  CoreDataStack.swift
+//  SkinCareList
+//
+//  Created by angelique fourny on 30/03/2023.
+//
+
+import Foundation
+import CoreData
+
+final class CoreDataStack {
+    
+    // MARK: - Properties
+    
+    private let persistentContainerName = "SkinCareList"
+    
+    // MARK: - Singleton
+    
+    static let sharedInstance = CoreDataStack()
+    
+    // MARK: - Public
+    
+    var viewContext: NSManagedObjectContext {
+        return CoreDataStack.sharedInstance.persistentContainer.viewContext
+    }
+    
+    // MARK: - Private
+    
+    private init() {}
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: persistentContainerName)
+        container.loadPersistentStores(completionHandler: {storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo) for: \(storeDescription.description)")
+            }
+        })
+        return container
+    }()
+}
