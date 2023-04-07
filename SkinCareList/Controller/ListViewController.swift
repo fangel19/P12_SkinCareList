68d45276da4d618910322c86b46157d4e8866077
fangel19 <fournyangel@msn.com>
2023-04-07 20:36:15 +0200

    Modification de contraintes

--- SkinCareList/Controller/ListViewController.swift
+++ SkinCareList/Controller/ListViewController.swift
@@ -88,6 +88,6 @@ extension ListViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 120
+        return 100
     }
 }
